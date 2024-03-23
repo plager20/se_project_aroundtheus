62edf7a4f69fcc41d945b38fc829916aa8233ad7
Gerald Planty <plantygerald02@gmail.com>
2024-03-23 15:40:36 -0400

    Sprint 4 Final Stage Completed

--- scripts/index.js
+++ scripts/index.js
@@ -2,39 +2,84 @@ const initialCards = [
   {
     name: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+    alt: "Picture of Yosemite Valley",
   },
   {
     name: "Lake Louise",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+    alt: "Picture of Yosemite Valley",
   },
   {
     name: "Bald Mountains",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+    alt: "Picture of Yosemite Valley",
   },
   {
     name: "Latemar",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+    alt: "Picture of Yosemite Valley",
   },
   {
     name: "Vanoise National Park",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+    alt: "Picture of Yosemite Valley",
   },
   {
     name: "Lago di Braies",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+    alt: "Picture of Yosemite Valley",
   },
 ];
 
-let editButton = document.querySelector(".profile__edit-button");
-let closeButton = document.querySelector(".modal__close");
-let modal = document.querySelector(".modal");
-
-function openModal() {
-  modal.classList.add("modal_opened");
-}
-editButton.addEventListener("click", openModal);
+const profileEditButton = document.querySelector(".profile__edit-button");
+const profileEditModal = document.querySelector(".modal");
+const profileEditCloseButton = profileEditModal.querySelector(".modal__close");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardListEL = document.querySelector(".cards__list");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
 
 function closeModal() {
-  modal.classList.remove("modal_opened");
+  profileEditModal.classList.remove("modal_opened");
 }
-closeButton.addEventListener("click", closeModal);
+
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+
+  const cardImageEL = cardElement.querySelector(".card__image");
+  cardImageEL.src = cardData.link;
+
+  const cardImageAltEL = cardElement.querySelector(".card__image");
+  cardImageAltEL.textContent = cardData.name;
+
+  const cardTitleEL = cardElement.querySelector(".card__title");
+  cardTitleEL.textContent = cardData.name;
+
+  return cardElement;
+}
+
+profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
+  profileEditModal.classList.add("modal_opened");
+});
+
+profileEditCloseButton.addEventListener("click", closeModal());
+
+profileEditForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closeModal();
+});
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListEL.append(cardElement);
+});
