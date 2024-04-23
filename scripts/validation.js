f7bbe9666cb9f4578490a05411c3ae1d2360386d
Gerald Planty <plantygerald02@gmail.com>
2024-04-23 18:53:37 -0400

    Corrections

--- scripts/validation.js
+++ scripts/validation.js
@@ -38,7 +38,7 @@ function toggleButtonState(inputEls, submitButton, { inactiveButtonClass }) {
 function setEventListeners(formEl, options) {
   const { inputSelector } = options;
   const inputEls = [...formEl.querySelectorAll(inputSelector)];
-  const submitButton = formEl.querySelector(".modal__button");
+  const submitButton = formEl.querySelector(options.submitButtonSelector);
   inputEls.forEach((inputEl) => {
     inputEl.addEventListener("input", (e) => {
       checkInputValidity(formEl, inputEl, options);
